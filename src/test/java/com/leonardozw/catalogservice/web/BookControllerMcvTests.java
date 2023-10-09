0748e492a748d82270a8f92e544a20923fd20dea
leonardo <leonardozw46@gmail.com>
2023-10-09 17:15:49 -0300

    feat: security propagation implemented

--- src/test/java/com/leonardozw/catalogservice/web/BookControllerMcvTests.java
+++ src/test/java/com/leonardozw/catalogservice/web/BookControllerMcvTests.java
@@ -1,6 +1,7 @@
 package com.leonardozw.catalogservice.web;
 
 import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -8,26 +9,66 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.leonardozw.catalogservice.config.SecurityConfig;
 import com.leonardozw.catalogservice.domain.BookNotFoundException;
 import com.leonardozw.catalogservice.domain.BookService;
 
 @WebMvcTest(BookController.class)
+@Import(SecurityConfig.class)
 public class BookControllerMcvTests {
-    
+
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    JwtDecoder jwtDecoder;
 
     @MockBean
     private BookService bookService;
 
     @Test
-    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
-        String isbn = "73737313940";
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204() throws Exception{
+        var isbn = "7373731394";
+        mockMvc
+            .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                .with(SecurityMockMvcRequestPostProcessors.jwt()
+                    .authorities(new SimpleGrantedAuthority("ROLE_employee"))))
+            .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403() throws Exception{
+        var isbn = "7373731394";
+        mockMvc
+            .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                .with(SecurityMockMvcRequestPostProcessors.jwt()
+                    .authorities(new SimpleGrantedAuthority("ROLE_customer"))))
+            .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void whenDeleteBookNotAuthenticatedThenShouldReturn401()throws Exception{
+        var isbn = "7373731394";
+        mockMvc
+            .perform(MockMvcRequestBuilders.delete("/books/" + isbn))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    void whenGetBookNotExistingAndAuthenticatedThenShouldReturn404() throws Exception {
+        var isbn = "7373731394";
         given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
         mockMvc
-            .perform(get("/books/" + isbn))
-            .andExpect(status().isNotFound());
+                .perform(get("/books/" + isbn)
+                        .with(jwt()))
+                .andExpect(status().isNotFound());
     }
 }
